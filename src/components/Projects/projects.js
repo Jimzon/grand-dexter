47bdfd4175e0cb1eddfe40e8526ed454ff943b2a
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-29 00:33:56 +0800

    projects imgs

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./projects.css";
-import Portfolio1 from "../../assets/portfolio-1.png";
-import Portfolio2 from "../../assets/portfolio-2.png";
-import Portfolio3 from "../../assets/portfolio-3.png";
+import Portfolio1 from "../../assets/project1.png";
+import Portfolio2 from "../../assets/project2.webp";
+import Portfolio3 from "../../assets/project3.jpg";
 import Portfolio4 from "../../assets/portfolio-4.png";
 import Portfolio5 from "../../assets/portfolio-5.png";
 import Portfolio6 from "../../assets/portfolio-6.png";
@@ -10,17 +10,18 @@ import Portfolio6 from "../../assets/portfolio-6.png";
 const Projects = () => {
   return (
     <section id="projects">
-      <h2 className="projectsTitle">My Portfolio</h2>
-      <span>
-        I take pride on my works as UI/UX designer and fullstack web developer
+      <h2 className="projectsTitle">My Projects</h2>
+      <span className="projectsDesc">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris feugiat
+        rhoncus diam et eleifend. Maecenas mattis.
       </span>
       <div className="projectsImgs">
         <img className="projectsImg" src={Portfolio1} alt="" />
         <img className="projectsImg" src={Portfolio2} alt="" />
         <img className="projectsImg" src={Portfolio3} alt="" />
-        <img className="projectsImg" src={Portfolio4} alt="" />
+        {/* <img className="projectsImg" src={Portfolio4} alt="" />
         <img className="projectsImg" src={Portfolio5} alt="" />
-        <img className="projectsImg" src={Portfolio6} alt="" />
+        <img className="projectsImg" src={Portfolio6} alt="" /> */}
       </div>
       <button className="projectBtn">See More</button>
     </section>
