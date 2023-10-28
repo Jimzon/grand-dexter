51df90202e31b04357e480c51384be8152244172
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-28 23:41:20 +0800

    projects js

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,7 +1,30 @@
 import React from "react";
+import "./projects.css";
+import Portfolio1 from "../../assets/portfolio-1.png";
+import Portfolio2 from "../../assets/portfolio-2.png";
+import Portfolio3 from "../../assets/portfolio-3.png";
+import Portfolio4 from "../../assets/portfolio-4.png";
+import Portfolio5 from "../../assets/portfolio-5.png";
+import Portfolio6 from "../../assets/portfolio-6.png";
 
 const Projects = () => {
-  return <div>test</div>;
+  return (
+    <section id="projects">
+      <h2 className="projectsTitle">My Portfolio</h2>
+      <span>
+        I take pride on my works as UI/UX designer and fullstack web developer
+      </span>
+      <div className="projectsImgs">
+        <img className="projectsImg" src={Portfolio1} alt="" />
+        <img className="projectsImg" src={Portfolio2} alt="" />
+        <img className="projectsImg" src={Portfolio3} alt="" />
+        <img className="projectsImg" src={Portfolio4} alt="" />
+        <img className="projectsImg" src={Portfolio5} alt="" />
+        <img className="projectsImg" src={Portfolio6} alt="" />
+      </div>
+      <button className="projectBtn">See More</button>
+    </section>
+  );
 };
 
 export default Projects;
