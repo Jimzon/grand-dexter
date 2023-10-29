9f4b584667f284fdb511bd155a1eb09788162426
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-29 15:25:34 +0800

    import dexter profile img

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,16 +1,20 @@
 import React from "react";
+import AboutPic from "../../assets/dexter.png";
 
 const About = () => {
   return (
     <section id="about">
       <div className="aboutContent">
-        <span className="aboutText"></span>
+        <span className="aboutText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          feugiat rhoncus diam et eleifend. Maecenas mattis.
+        </span>
         <p className="introPara">
           I'm a skill web developer and UI/UX designer
         </p>
         <a href="" />
       </div>
-      <img className="bg" src="" alt="" />
+      <img className="bg" src={AboutPic} alt="" />
       Hire Me
     </section>
   );
