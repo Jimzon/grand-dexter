b7404a47b13269ff36a2239b69ff0f8ab0935a69
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-29 14:01:45 +0800

    about components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Intro from "./components/Intro/intro";
 import Projects from "./components/Projects/projects";
+import About from "./components/About/about";
 
 function App() {
   return (
     <div>
       <Intro />
       <Projects />
+      <About />
     </div>
   );
 }
---
+++ src/components/About/about.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section id="about">
+      <div className="aboutContent">
+        <span className="aboutText"></span>
+        <p className="introPara">
+          I'm a skill web developer and UI/UX designer
+        </p>
+        <a href="" />
+      </div>
+      <img className="bg" src="" alt="" />
+      Hire Me
+    </section>
+  );
+};
+
+export default About;
