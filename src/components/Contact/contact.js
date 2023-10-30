41e72ce20f75a8031d6d7efa3d3692f9a6b53e92
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-30 13:47:26 +0800

    contacts component

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./contact.css";
+
+const Contact = () => {
+  return (
+    <section id="stacks">
+      <div className="stacksContent">
+        <h2 className="stacksTitle">My Stacks</h2>
+      </div>
+      <div className="stacksIcon"></div>
+    </section>
+  );
+};
+
+export default Contact;
