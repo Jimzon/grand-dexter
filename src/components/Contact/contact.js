086da06af590c7ae1f4907da9b4cef85a69c426d
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-30 15:23:21 +0800

    contact modified

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -3,22 +3,41 @@ import "./contact.css";
 
 const Contact = () => {
   return (
-    <section id="contact">
-      <div className="contactContent">
-        <h2 className="contactTitle">About Me</h2>
-        <p className="contactPara">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-          feugiat rhoncus diam et eleifend. Maecenas mattis.
-        </p>
-        <div className="resumeDiv">
-          <p className="resume">View my</p>
-          <a href="" className="dexter">
-            <span>Resume</span>
-          </a>
-        </div>
-      </div>
-      <div className="aboutImgDiv">
-        <img className="aboutImg" src="" alt="" />
+    <section id="contactPage">
+      <div className="contact">
+        <h1 className="contactPageTitle">Contact Me</h1>
+        <span className="contactDec">
+          Please fill out the form below to discuss any work opportunities
+        </span>
+        <form className="contactForm">
+          <input
+            className="name"
+            type="text"
+            placeholder="Your Name"
+            name="your_name"
+          />
+          <input
+            className="email"
+            type="email"
+            placeholder="Your Email"
+            name="from_email"
+          />
+          <textarea
+            className="msg"
+            name="message"
+            rows="5"
+            placeholder="Your Message"
+          ></textarea>
+          <button className="submitBtn" type="submit" value="Send">
+            Submit
+          </button>
+          {/* <div className="links">
+            <img className="link" src={FacebookIcon} alt="Facebook" />
+            <img className="link" src={TwitterIcon} alt="Twitter" />
+            <img className="link" src={YouTubeIcon} alt="YouTube" />
+            <img className="link" src={InstagramIcon} alt="Instagram" />
+          </div> */}
+        </form>
       </div>
     </section>
   );
